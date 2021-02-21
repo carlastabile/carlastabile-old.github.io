3b5b36613358f9767f36bc530ebbc35ea6a6e498
Carla Urrea Stabile <carla.urrea@xing.com>
2021-02-21 17:17:19 +0100

    fetch medium posts

--- js/main.js
+++ js/main.js
@@ -61,4 +61,48 @@ Menu
 
 })();
 
+/******************************************************************************************************************************
+fetch posts
+*******************************************************************************************************************************/ 
+fetch('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@carlastabile')
+   .then((res) => res.json())
+   .then((data) => {
+      // Filter for acctual posts. Comments don't have categories, therefore can filter for items with categories bigger than 0
+      const res = data.items //This is an array with the content. No feed, no info about author etc..
+      const posts = res.filter(item => item.categories.length > 0) // That's the main trick* !
 
+      // Functions to create a short text out of whole blog's content
+      function toText(node) {
+         let tag = document.createElement('div')
+         tag.innerHTML = node
+         node = tag.innerText
+         return node
+      }
+      function shortenText(text,startingPoint ,maxLength) {
+         return text.length > maxLength?
+         text.slice(startingPoint, maxLength):
+         text
+      }
+
+      // Put things in right spots of markup
+      let output = '';
+      posts.forEach((item) => {
+         output += `
+         <li>
+            <a href="${item.link}">
+	            <div class="col-md-6">
+		             <div class="overlay-effect effects clearfix">
+		               <div class="img">
+		                 <img src="${item.thumbnail}" class="grayscale" style="width:100%;height:300px">
+		               </div>
+		             </div>
+	               <br>
+	               <h2>${item.title}</h2>
+	               <p>${shortenText(toText(item.content),60, 300)+ '...'}</p>
+              </div>
+            <a/>
+         </li>`
+
+      })
+      document.querySelector('.slides').innerHTML = output
+});
